3c8c1dc32a86c61cddd78560cbf0b0e9195337b3
qiangtou <qiangtou@gmail.com>
2013-07-06 22:20:05 +0800

    events

--- base.js
+++ base.js
@@ -202,4 +202,137 @@
 		}
 	}
 	window['ADS']['walkTheDOMWithAttributes']=walkTheDOMWithAttributes;
+	function addLoadEvent(loadEvent,waitForImages){
+		if(!isComparible()){return false;}
+		//如果等待标记是true则使用常规的添加事件的方法
+		if(waitForImages){
+			return addEvent(window,'load',loadEvent);
+		}
+		//否则使用一些不同的方式包装loadEvent()方法，以便为this关键字指定正确的内容，
+		//同时确保事件不会被招待两次
+		var init =function(){
+			//如果这个函数已经被调用过了则返回
+			if(arguments.callee.done)return;
+			//标记这个函数以便检验它是否运行过
+			arguments.callee.done=true;
+			//在document的环境中运行载入事件
+			loadEvent.apply(document,arguments);
+		};
+		//为DOMContentLoaded事件注册事件侦听器
+		if(document.addEventListener){
+			document.addEventListener('DOMContentLoaded',init,false);
+		}
+		//对于Safari,使用setInterval()函数检测document是否载入完成
+		if(/webkit/i.test(navigator.userAgent)){
+			var _timer=setInterval(function(){
+				if(/loaded|complete/.test(document.readyState))	{
+					clearInterval(_timer);
+					init();
+				}
+			},10);
+		}
+		//对于IE，附加一个在载入过程最后招待的脚本，并检测该脚本是否载入完成
+		/*@cc_on @*/
+		/*if (@_win32)
+		  document.write('<script id="__ie_onload" defer src="javascript:void(0)" type="text/javascript"><\/script>');
+		  var script=document.getElementById('__ie_onload');
+		  script.onreadystatechange=function(){
+		  if(this.readyState=='complete'){
+		  init();
+		  }
+		  }
+		/*@end @*/
+		return true;
+	}
+	window['ADS']['addLoadEvent']=addLoadEvent;
+	function stopPropagetion(eventObject){
+		eventObject=eventObject||getEventObject(eventObject);
+		if(eventObject.stopPropagetion){
+			eventObject.stopPropagetion();
+		}else{
+			eventObject.cancelBubble=true;
+		}
+	}
+	window['ADS']['stopPropagetion']=stopPropagetion;
+	function preventDefault(eventObject){
+		eventObject=eventObject||getEventObject(eventObject);
+		if(eventObject.preventDefault){
+			eventObject.preventDefault();
+		}else{
+			eventObject.returnValue=false;
+		}
+	}
+	window['ADS']['preventDefault']=preventDefault;
+	function getEventObject(W3CEvent){
+		return W3CEvent||window.event;
+	}
+	window['ADS']['getEventObject']=getEventObject;
+	function getTarget(eventObject){
+		eventObject=eventObject||getEventObject(eventObject);
+		//如果是W3C或MSIE的模型
+		var target=eventObject.target||eventObject.srcElement;
+		//如果像Safari中一样是一个文本节点
+		//重新将目标对象指定为父元素
+		if(target.nodeType==ADS.TEXT_NODE){
+			target= node.parentNode;
+		}
+		return target;
+	}
+	window['ADS']['getTarget']=getTarget;
+	function getMouseButton(eventObject){
+		eventObject=eventObject||getEventObject(eventObject);
+		//使用适当的属性初始化一个对象变量
+		var buttons={
+			left:false,
+			middle:false,
+			right:false
+		};
+		//检查eventObject对象的toString()方法的值
+		//W3C DOM 对象有toString方法并且此时该方法返回值应该是MouseEvent
+		if(eventObject.toString && eventObject.toString().indexOf('MouseEvent')!=-1){
+			//W3C方法
+			switch(eventObject.button){
+				case 0:buttons.left=true;break;
+				case 1:buttons.middle=true;break;
+				case 2:buttons.right=true;break;
+				default:break;
+			}
+		}else if(eventObject.button){
+			switch(eventObject.button){
+				case 1:buttons.left=true;break;
+				case 2:buttons.right=true;break;
+				case 3:
+				       buttons.left=true;
+				       buttons.right=true;
+				       break;
+				case 4:button.middle=true;break;
+				case 5:
+				       buttons.left=true;
+				       buttons.middle=true;
+				       break;
+				case 6:
+				       buttons.middle=true;
+				       buttons.right=true;
+				       break;
+				case 7:
+				       buttons.left=true;
+				       buttons.middle=true;
+				       buttons.right=true;
+				       break;
+				default:break;
+			}
+		}else{
+			return false;
+		}
+	return buttons;	
+	}
+	window['ADS']['getMouseButton']=getMouseButton;
+	function getPointerPositionInDocument(eventObject){
+		eventObject=eventObject||getEventObject(eventObject);
+		var x=eventObject.pageX||(eventObject.clientX+(document.documentElement.scrollLeft||document.body.scrollLeft));
+		var y=eventObject.pageY||(eventObject.clientY+(document.documentElement.scrollTop||document.body.scrollTop));
+		//现在x和y中包含着鼠标相对于文档原点的坐标
+		return {x:x,y:y}; 
+	}
+	window['ADS']['getPointerPositionInDocument']=getPointerPositionInDocument;
 })()
